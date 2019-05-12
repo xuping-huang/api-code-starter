7e5d6949d5eb873973dbd751f36195a759bf1a9a
Xuping Huang <xuping.huang.jason@gmail.com>
2019-05-12 11:13:42 +0800

    test helper enchance

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Debug from 'debug';
 import path from 'path';
-
+import * as _ from 'lodash';
 import * as generator from './lib/generator';
 import * as nodeConfig from './lib/yaml-config';
 
@@ -9,9 +9,12 @@ const debug = Debug('code:main');
 
 const main = () => {
   const codeEnv = process.env.CODE_ENV || 'development';
+  const nameEnv = process.env.NAME_ENV || 'project-name';
   const configEnv = process.env.CONFIG_ENV || 'common.yml'
   const configFile = path.resolve(__dirname, `../config/${configEnv}`);
   const config = nodeConfig.load(configFile, codeEnv);
+  config.project.name = nameEnv;
+  config.project.description = _.startCase(nameEnv);
   debug(config);
 
   return generator.run(config);
